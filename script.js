3643d6d29b5ada339632b21e0b6391721ac9c0d2
davimoljo <davi.moljo@estudante.ufjf.br>
2025-04-10 17:03:57 -0300

    starting JS

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello, World!");
+document.getElementById("inicio").addEventListener("click", scrollToSectionInicio);
+    
+  function scrollToSectionInicio() {
+    document.getElementById("first-container").scrollIntoView({
+      behavior: "smooth"
+    });
+  }
+
+  
+  document.getElementById("sobre").addEventListener("click", scrollToSectionSobre);
+  function scrollToSectionSobre() {
+    document.getElementById("aboutus-container").scrollIntoView({
+      behavior: "smooth"
+    });
+  }
+  document.getElementById("contato").addEventListener("click", scrollToSectionContato);
+  function scrollToSectionContato() {
+    document.getElementById("contact-us").scrollIntoView({
+      behavior: "smooth"
+    });
+  }
